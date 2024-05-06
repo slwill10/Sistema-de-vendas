b7b411d693d15b4d534e3ad3d89ef00f4599eda7
Willian Santos <wsantosdelima44@gmail.com>
2024-05-06 13:25:51 -0300

    método que lista itens vendidos

--- src/main/java/br/com/projeto/dao/ItemVendaDAO.java
+++ src/main/java/br/com/projeto/dao/ItemVendaDAO.java
@@ -5,10 +5,16 @@
 package br.com.projeto.dao;
 
 import br.com.projeto.jdbc.ConnectionFactory;
+import br.com.projeto.model.Clientes;
 import br.com.projeto.model.ItemVenda;
+import br.com.projeto.model.Produtos;
 import br.com.projeto.model.Vendas;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -40,5 +46,41 @@ public class ItemVendaDAO {
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Erro: " + e);
         }
+    }
+
+    // Método que lista itens de uma venda por id
+    public List<ItemVenda> listaItensPorVenda(int venda_id) {
+
+        List<ItemVenda> lista = new ArrayList<>();
+
+        try {
+            String sql = "select i.id, p.descricao, i.qtd, p.preco, i.subtotal from tb_itensvendas as i"
+                    + "inner join tb_produtos as p on (i.produto_id = p.id)"
+                    + "where i.venda_id = ?;";
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt(1, venda_id);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                
+                ItemVenda item = new ItemVenda();
+                Produtos prod = new Produtos();
+                
+                item.setId(rs.getInt("i.id"));
+                prod.setDescricao(rs.getString("p.descricao"));
+                item.setQtd(rs.getInt("i.qtd"));
+                prod.setPreco(rs.getDouble("p.preco"));
+                item.setSubtotal(rs.getDouble("i.subtotal"));
+                
+                item.setProduto(prod);
+                
+
+                lista.add(item);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error: " + e);
+        }
+        return lista;
     }
 }
